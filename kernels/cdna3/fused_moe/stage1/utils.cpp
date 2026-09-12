33c0456b3c8813bd69d36d82a52568f721a54085
Parxd <your-email@example.com>
2026-09-12 15:45:56 -0400

    fix store sentinel bug

--- kernels/cdna3/fused_moe/stage1/utils.cpp
+++ kernels/cdna3/fused_moe/stage1/utils.cpp
@@ -361,10 +361,7 @@ __device__  inline void scatter_store(
     const int total_bytes = row_stride * dst.rows() * sizeof(T);
     i32x4 srsrc = make_srsrc(base_ptr, total_bytes, row_stride_bytes);
 
-    // if (threadIdx.x == 128 && blockIdx.x == 0) {
-    //     printf("n_coord=%d\n", unit_coord.c);
-    // }
-    
+    const int sentinel = dst.rows() / TOP_K;  // since dst.rows = M * TOP_K
     #pragma unroll
     for (int i = 0; i < RT::height; ++i) {
         #pragma unroll
@@ -373,24 +370,13 @@ __device__  inline void scatter_store(
             #pragma unroll
             for (int k = 0; k < 4; ++k) {
                 int row_offset = (i * 16) + (kittens::laneid() / 16 * 4) + k;
-                int phys_row = unit_coord.r + row_offset;
                 int col_offset = (j * 16) + (kittens::laneid() % 16);
                 int packed = tm_ptr[row_offset];  // no raw buffer load here b/c row_offset guaranteed to land in valid tiles
                 int token_id = packed & 0x00FFFFFF;
                 int topk_slot = (packed & 0xFF000000) >> 24;
+                int flat_offset = (token_id * TOP_K + topk_slot) * row_stride + col_offset;
 
-                // if (threadIdx.x >= 64 && threadIdx.x < 127 && !blockIdx.x) {
-                //     printf("TID: %d, frg. %d: %d\n", threadIdx.x, k, token_id);
-                // }
-                int flat_offset = (phys_row * TOP_K + topk_slot) * row_stride + col_offset;
-                int byte_offset = flat_offset * sizeof(T);
-
-                // if (threadIdx.x == 128 && blockIdx.x == 0) {
-                //     printf("TID %d: TOKEN %d, row %d\n", threadIdx.x, token_id, phys_row * TOP_K + topk_slot);
-                //     printf("col_offset = %d\n", col_offset);
-                // }
-
-                if (token_id != dst.rows()) {
+                if (token_id != sentinel) {
                     base_ptr[flat_offset] = __float2bfloat16(flat[k]);
                 }
             }
